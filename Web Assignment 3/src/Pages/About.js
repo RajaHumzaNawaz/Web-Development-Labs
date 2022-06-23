9d346112ed1b50073f2e4f155ab6ad467014063f
RajaHumzaNawaz <77897318+RajaHumzaNawaz@users.noreply.github.com>
2022-06-23 13:09:45 -0700

    Add files via upload

---
+++ Web Assignment 3/src/Pages/About.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './About.css';
+
+const About = () => {
+  return (
+    <div id="about"  data-aos="zoom-in" data-aos-duration="1200">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-11 offset-sm-1">
+            <div className="aboutcontent">
+              <h2>About Me</h2>
+              <p>An enthusiastic and upbeat individual ready to accept a new challenge where my education, skills, and personality will provide growth and support to an organization.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
